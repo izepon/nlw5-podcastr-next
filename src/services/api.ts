06259544e9b14ae852c0c7d85ef6fd822d82d981
izepon <izeponjc@gmail.com>
2021-04-24 18:49:29 -0300

    db.json

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,11 @@
+// import axios from "axios";
+
+// export const api = axios.create({
+//   baseURL: process.env.API_URL || "http://localhost:3333/",
+// });
+
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "https://my-json-server.typicode.com/izepon/nlw5-podcastr-next",
 });
